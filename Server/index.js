469cd266562b1c411d160b1569a0127579903b8a
JRhymes7 <jrhymes7@gatech.edu>
2025-07-24 13:48:55 -0400

    Update mongoose.connect to use env variable

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/get', (req,res) => {
     TodoModel.find()
